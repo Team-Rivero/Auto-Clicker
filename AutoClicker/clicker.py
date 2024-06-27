319eb0f967248cfe03f655d1570f00c488e11e24
Rivero <Rivero7462@gmail.com>
2024-06-26 22:18:54 -0500

    Update clicker.py
    Used grids for the items, and attempting to get key numbers to be the same and work for all keys

--- AutoClicker/clicker.py
+++ AutoClicker/clicker.py
@@ -3,11 +3,7 @@ import time
 import threading
 from pynput import mouse, keyboard
 
-# button = mouse.Button.right
-#Default Input
-#button_to_click = 'e'
-#Default Trigger Input Key
-start_stop_key = keyboard.KeyCode(char='a')
+start_stop_key = keyboard.KeyCode(char='')
 
 #Center Window on screen
 def center_window(window):
@@ -24,13 +20,16 @@ def center_window(window):
 
 #Define Trigger Button DOESN'T WORK FOR MOUSE BUTTONS YET
 def on_single_key(e):
+    
     global start_stop_key
 
     window.unbind("<KeyPress>")
     start_stop_key = keyboard.KeyCode(char=chr(e.keycode).lower())
     input_trigger.config(text=str(ord(start_stop_key.char)) + " | " + start_stop_key.char)
+    #print(start_stop_key)
 
-#Create Window
+#region Created Windows
+#Main Window
 window = Tk()
 window.title("Clicker")
 window.wm_attributes("-topmost", 1)
@@ -41,51 +40,52 @@ frame.pack()
 
 #Create Input Trigger Input
 label_trigger = Label(frame, text="Trigger: ")
-label_trigger.pack(side=LEFT, padx=0, pady=0)
+label_trigger.grid(row=0, column=0, padx=(5, 0), pady=(5, 0))
 
-input_trigger = Button(frame, text=str(ord(start_stop_key.char)) + " | " + start_stop_key.char)
-input_trigger.pack(side=LEFT, padx=(0, 15), pady=0)
+input_trigger = Button(frame, text="Set Trigger Key")
+input_trigger.grid(row=0, column=1, padx=(0, 15), pady=(5, 0))
 input_trigger.config(command=lambda: (window.bind("<KeyPress>", on_single_key), input_trigger.config(text=" ")))
-
+ 
 #Button To Click Input
-label_click = Label(frame, text="Key to Click: ")
-label_click.pack(side=LEFT, padx=0, pady=0)
+label_click = Label(frame, text="Button to Click: ")
+label_click.grid(row=2, column=0, padx=(5, 0), pady=(5, 0))
 
-button_to_click = Text(frame, height = 1, width = 3)
-button_to_click.pack(side=LEFT, padx=(0, 15), pady=0)
+button_to_click = Text(frame, height=1, width=3)
+button_to_click.grid(row=2, column=1, padx=(0, 15), pady=(5, 0))
 button_to_click.insert("1.0", "e")
 
 #Create Click-Rate Input
 label_rate = Label(frame, text="Clicks/s: ")
-label_rate.pack(side=LEFT, padx=0, pady=0)
+label_rate.grid(row=0, column=2, padx=(5, 0), pady=(5, 0))
 
-click_rate = Text(frame, height = 1, width = 3)
-click_rate.pack(side=LEFT, padx=(0, 15), pady=0)
-click_rate.insert("1.0", "1")
+click_rate = Text(frame, height=1, width=3)
+click_rate.grid(row=0, column=3, padx=(0, 15), pady=(5, 0))
+click_rate.insert("1.0", "100")
 
 #Create Duration Time
-label_duration = Label(frame, text="Duration in Seconds: ")
-label_duration.pack(side=LEFT, padx=0, pady=0)
+label_duration_time = Label(frame, text="Duration in Seconds: ")
+label_duration_time.grid(row=1, column=2, padx=(5, 0), pady=(5, 0))
 
-duration_time = Text(frame, height = 1, width = 3)
-duration_time.pack(side=LEFT, padx=(0, 15), pady=0)
+duration_time = Text(frame, height=1, width=3)
+duration_time.grid(row=1, column=3, padx=(0, 15), pady=(5, 0))
 duration_time.insert("1.0", "0")
 
 #Create Duration Clicks
-label_duration_clicks = Label(frame, text="Duraction in Clicks: ")
-label_duration_clicks.pack(side=LEFT, padx=0, pady=0)
+label_duration_clicks = Label(frame, text="Duration in Clicks: ")
+label_duration_clicks.grid(row=2, column=2, padx=(5, 0), pady=(5, 0))
 
-duration_clicks = Text(frame, height = 1, width = 3)
-duration_clicks.pack(side=LEFT, padx=(0, 15), pady=0)
+duration_clicks = Text(frame, height=1, width=3)
+duration_clicks.grid(row=2, column=3, padx=(0, 15), pady=(5, 0))
 duration_clicks.insert("1.0", "0")
 
-#Create Toggle/Hold Checkbox
+# Create Toggle/Hold Checkbox
 label_toggle = Label(frame, text="Toggle/Hold: ")
-label_toggle.pack(side=LEFT, padx=0, pady=0)
+label_toggle.grid(row=0, column=4, padx=(5, 0), pady=(5, 0))
 
 toggle_checkbox = BooleanVar(value=True)
 toggle_box = Checkbutton(frame, variable=toggle_checkbox)
-toggle_box.pack(side=LEFT, padx=0, pady=0)
+toggle_box.grid(row=0, column=5, padx=(0, 15), pady=(5, 0))
+#endregion
 
 #Thread to control clicks
 class ClickButton(threading.Thread):
@@ -98,8 +98,8 @@ class ClickButton(threading.Thread):
     def run(self):
         while self.program_running:
             while self.running:
-                #mouse.click(self.button.left)
-                controller.press(self.button_to_click)
+                #mouse_controller.click(self.button.left)
+                keyboard_controller.press(self.button_to_click)
                 time.sleep(1/self.click_rate)
             time.sleep(0.1)
 
@@ -108,34 +108,58 @@ class ClickButton(threading.Thread):
         self.program_running = False
 
 # instance of mouse and keyboard controller created
-mouse = mouse.Controller()
-controller = keyboard.Controller()
+mouse_controller = mouse.Controller()
+keyboard_controller = keyboard.Controller()
+
+#Checks if pressed key is input trigger key to start/stop
+def on_press(key):
+    if str(start_stop_key) != "''":
+        try:
+            # Check if the pressed key has a char attribute (alphanumeric keys)
+            char = ord(key.char)
+        except AttributeError:
+            #Special keys like 'Key.space', 'Key.enter', 'Key.backspace', etc.
+            char = int(str(key.value).replace("'", "").replace("<", "").replace(">", ""))
+
+            #Modifier Keys
+            if char == 164 or char == 165:
+                #Alt left and right
+                char = 18
+            elif char == 162 or char == 163:
+                #Ctrl left and right
+                char = 17
+            elif char == 160 or char == 161:
+                #Shift left and right
+                char = 16
+            #elif char == '':
+            #    char = 32
+            
+        #print(char)
+        #print(ord(start_stop_key.char))
+
+        if char == ord(start_stop_key.char):
+            if click_thread.running:
+                click_thread.running = False
+            else:
+                #apply settings before starting
+                click_thread.button_to_click = (button_to_click.get("1.0", END).strip())
+                click_thread.click_rate = float(click_rate.get("1.0", END).strip())
+                click_thread.running = True
+                # Duration handling
+                duration_sec = float(duration_time.get("1.0", END).strip())
+                if duration_sec > 0:
+                    # Schedule a stop after duration_sec seconds
+                    threading.Timer(duration_sec, lambda: setattr(click_thread, 'running', False)).start()
 
 #Create Thread
 click_thread = ClickButton()
 click_thread.start()
 
-#Checks if pressed key is input trigger key to start/stop
-def on_press(key):
-    if key == start_stop_key:
-        if click_thread.running:
-            click_thread.running = False
-        else:
-            #apply settings before starting
-            click_thread.button_to_click = (button_to_click.get("1.0", END).strip())
-            click_thread.click_rate = float(click_rate.get("1.0", END).strip())
-            click_thread.running = True
-            # Duration handling
-            duration_sec = float(duration_time.get("1.0", END).strip())
-            if duration_sec > 0:
-                # Schedule a stop after duration_sec seconds
-                threading.Timer(duration_sec, lambda: setattr(click_thread, 'running', False)).start()
-
 # Create a listener instance
-listener = keyboard.Listener(on_press=on_press)
+keyboard_listener = keyboard.Listener(on_press=on_press)
 
 # Start the listener in a separate thread
-listener_thread = threading.Thread(target=listener.start)
+listener_thread = threading.Thread(target=keyboard_listener.start)
 listener_thread.start()
 
 #Stop separate thread on window close
